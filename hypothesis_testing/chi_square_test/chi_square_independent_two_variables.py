93d0a870999fd295394b16a4d818a5361f7f5a23
qiushi.xu <qiushi.xu@17zuoye.com>
2021-04-14 14:40:06 +0800

    增加卡方双变量比例检验

--- hypothesis_testing/chi_square_test/chi_square_independent_two_variables.py
+++ hypothesis_testing/chi_square_test/chi_square_independent_two_variables.py
@@ -1,6 +1,11 @@
+import logging
+from typing import Optional
+
 import pandas as pd
 import numpy as np
 import scipy.stats
+
+from hypothesis_testing.chi_square_test.entity.chi_square_result import ChiSquareResult
 
 
 class ChiSquareIndependentTwoVariables:
@@ -12,12 +17,45 @@ class ChiSquareIndependentTwoVariables:
     配对变量不可以使用
     """
 
-    def __init__(self, array_1: np.ndarray, array_2: np.ndarray):
-        self.array_1 = array_1
-        self.array_2 = array_2
+    def __init__(
+        self, array_1: np.ndarray, array_2: np.ndarray, p_thread: float = 0.05
+    ):
+        self.array_1 = array_1  # 代表分类
+        self.array_2 = array_2  # 代表每个分类下的不同效果
+        self.p_thread = p_thread
+        logging.basicConfig(level=logging.INFO)
+        logging.info(f"{'ChiSquareIndependentTwoVariables':=^50}")
+        logging.info(f"卡方检验，需要保证格子中频数")
 
     def test(self):
-        cross_table = pd.crosstab(self.array_1, self.array_2)
-        statistic, p = scipy.stats.chisquare(cross_table[0], cross_table[1])
+        cross_table = self._get_cross_table()
+        condition_check_result = self._check_condition(cross_table)
+        if condition_check_result:
+            logging.info(f"格子频数满足要求")
+            statistic, p = scipy.stats.chisquare(cross_table[0], cross_table[1])
+            return ChiSquareResult(True, p, p <= self.p_thread)
+        else:
+            logging.info(f"格子数量不满足要求，使用Fisher exact test")
+            statistic, p = scipy.stats.fisher_exact(cross_table)
+            return ChiSquareResult(True, p, p <= self.p_thread)
 
-        pass
+    def _get_cross_table(self):
+        type_column = np.concatenate(
+            [np.zeros(self.array_1.shape[0]), np.ones(self.array_2.shape[0])]
+        )
+        value_column = np.concatenate([self.array_1, self.array_2])
+        return pd.crosstab(type_column, value_column).fillna(0)
+
+    @classmethod
+    def _check_condition(cls, cross_table: pd.DataFrame) -> bool:
+        if cross_table.values.sum() <= 40:
+            logging.warning(f"总频数小于40")
+            return False
+
+        if (cross_table < 5).values.sum() / (
+            cross_table.shape[0] * cross_table.shape[1]
+        ) >= 0.2:
+            logging.warning(f"小于5的格子数超过20%")
+            return False
+
+        return True
--- tests/test_hypothesis_testing/test_chi_square_testing/test_chi_square_independent_two_variables.py
+++ tests/test_hypothesis_testing/test_chi_square_testing/test_chi_square_independent_two_variables.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 from data_mocker.distribution_sampling import DistributionSampling
 from hypothesis_testing.chi_square_test.chi_square_independent_two_variables import (
     ChiSquareIndependentTwoVariables,
@@ -7,10 +9,14 @@ from hypothesis_testing.chi_square_test.chi_square_independent_two_variables imp
 
 
 class TestChiSquareIndependentTwoVariables(unittest.TestCase):
-    def test_chi_square_condition(self):
-        array_1 = DistributionSampling(seed=1).generate_bernoulli_samplings(0.7, 1000)
+    def test_chi_square(self):
+        array_1 = DistributionSampling(seed=1).generate_bernoulli_samplings(0.5, 1000)
         array_2 = DistributionSampling(seed=2).generate_bernoulli_samplings(0.6, 1000)
+        array_3 = DistributionSampling(seed=3).generate_bernoulli_samplings(0.5, 1000)
         test_result = ChiSquareIndependentTwoVariables(array_1, array_2).test()
+        self.assertTrue(test_result.rejected)
+        test_result = ChiSquareIndependentTwoVariables(array_1, array_3).test()
+        self.assertTrue(not test_result.rejected)
 
 
 if __name__ == "__main__":
